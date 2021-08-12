63638b8dc244d5254629bd7a7ebf54ebce6d0b39
SanEstra <sanestra@uw.edu>
2021-08-12 12:21:02 -0700

    Finished the column names with grouping, using expand.grid().

--- main_script.r
+++ main_script.r
@@ -2,6 +2,7 @@ library(here)  # here()
 library(dplyr) # various
 library(readr) # read_csv()
 library(purrr) # mapchr()
+library(tidyr)
 
 
 
@@ -14,9 +15,18 @@ COLUMN_GROUPS <- c("_Exist_BC", "_Exist_CC", "_Method", "Program_Type",
                    "_Coverage", "_Exist_HPV")
 
 
+# There is likely a better way to do this (i.e. purrr), but this works for a
+#   two variable solution for now, and gives the correct ordering.
+# Combine the groups years and groupings into a string so it can become a col
+#   name.
+
+grouped_colnames <- expand.grid(YEARS_OF_DATA, COLUMN_GROUPS) %>%
+    mutate(final_col_name = paste0(Var1, Var2)) %>%
+    pull(final_col_name)
+
+
 
 # Load in data.
 
 data <- read_csv(here("data.csv"), skip = 1)
-colnames(data) <- c("Country",
-                    paste0(YEARS_OF_DATA, )
+colnames(data) <- c("Country", grouped_colnames)
